d5d58e6d9fbb4123b6316713ea30f947782b47d2
Jamil Khalil <jamil7@dhaka.net>
2025-04-22 03:19:02 -0400

    Step 7: Add PerishableProductProperties class skeleton

---
+++ js/PerishableProductProperties.js
@@ -0,0 +1,8 @@
+// PerishableProductProperties.js
+
+import { ProductProperties } from './ProductProperties.js';
+
+// Subclass representing perishable products
+export class PerishableProductProperties extends ProductProperties {
+    // Constructor and methods will be added next
+}
