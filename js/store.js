fceba53bfae55e4aafd559602c1c22579ec006ad
Jamil Khalil <jamil7@dhaka.net>
2025-04-22 04:16:33 -0400

    Step 13: [Part 4 - Store Class] Add Store class skeleton with inventory

---
+++ js/store.js
@@ -0,0 +1,10 @@
+// Step 13: [Part 4 - Store Class] Define the Store class
+export class Store {
+    
+    // Step 13: [Part 4 - Store Class] Constructor initializes inventory array
+    constructor() {
+        this.inventory = [];
+    }
+
+    
+}
