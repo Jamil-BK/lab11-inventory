f51643c5f9643787abcf399f8c54f344f665d8cb
Jamil Khalil <jamil7@dhaka.net>
2025-04-22 03:26:57 -0400

    Step 8: Add constructor with super() and expirationDate

--- js/PerishableProductProperties.js
+++ js/PerishableProductProperties.js
@@ -4,5 +4,9 @@ import { ProductProperties } from './ProductProperties.js';
 
 // Subclass representing perishable products
 export class PerishableProductProperties extends ProductProperties {
-    // Constructor and methods will be added next
+    // Constructor to initialize inherited and new properties
+    constructor(name, price, quantity, expirationDate) {
+        super(name, price, quantity); // Call parent constructor
+        this.expirationDate = expirationDate; // New property
+    }
 }
