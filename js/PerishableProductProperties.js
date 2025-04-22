c6a3678115878f52a843331c14f510ef259db444
Jamil Khalil <jamil7@dhaka.net>
2025-04-22 03:32:09 -0400

    Step 9: [Part 2 - Inheritance] Override toString() to include expiration date

--- js/PerishableProductProperties.js
+++ js/PerishableProductProperties.js
@@ -6,7 +6,12 @@ import { ProductProperties } from './ProductProperties.js';
 export class PerishableProductProperties extends ProductProperties {
     // Constructor to initialize inherited and new properties
     constructor(name, price, quantity, expirationDate) {
-        super(name, price, quantity); // Call parent constructor
-        this.expirationDate = expirationDate; // New property
+        super(name, price, quantity);
+        this.expirationDate = expirationDate;
+    }
+
+    // Override toString() to include expiration date
+    toString() {
+        return `Product: ${this.name}, Price: $${this.price.toFixed(2)}, Quantity: ${this.quantity}, Expiration Date: ${this.expirationDate}`;
     }
 }
